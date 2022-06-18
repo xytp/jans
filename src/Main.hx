38e71c56784474472f1831d12808c2e5201fdeb7
Adrien Baudet <baudetad@gmail.com>
2022-06-18 13:25:31 +0200

    code optimization
    code optimization so the whole source directory is iterated but not saved anymore

--- src/Main.hx
+++ src/Main.hx
@@ -1,54 +1,54 @@
 package src;
 
-import sys.FileStat;
+import haxe.io.Path;
 import sys.io.File;
 import sys.FileSystem;
 
 using Lambda;
 
 class Main {
-    static public function main() {
-        var data = Sys.args()[0];
-        var content = File.getContent(data);
-        var obj: src.DataFormat = haxe.Json.parse(content);
-        var files = getFiles(obj.path, obj.extension);
-        Sys.println("Starting observation...");
-        while (true) {
-            var nfiles = getFiles(obj.path, obj.extension);
-            for (fi in nfiles.keyValueIterator()) {
-                if (!files.exists(fi.key) || (files[fi.key].mtime.getTime() != fi.value.mtime.getTime())) {
-                    Sys.println("File change detected. Compiling...");
-                    Sys.command(obj.command);
-                    Sys.println("Done.");
-                    files = nfiles;
-                    break;
-                }
-            }
-            Sys.sleep(obj.delay);
-        }
-    }
+	static private var now:Date = Date.now();
 
-    static private function getFiles(path : String, extension: Array<String>) : Map<String, FileStat> {
-        var dirs : Array<String> = [];
-        var files = new Map<String, FileStat>();
-        FileSystem.readDirectory(path)
-            .iter(function (name: String) {
-                var tpath = '${path}/${name}';
-                var ext = name.split(".");
-                if (FileSystem.isDirectory(tpath)) {
-                    dirs.push(tpath);
-                }
-                else if (extension.has(ext[ext.length - 1])) {
-                    var stat = FileSystem.stat(tpath);
-                    files.set(tpath, stat);
-                }
-            }
-        );
-        dirs.iter(function (name: String) {
-            for (fi in getFiles(name, extension).keyValueIterator()) {
-                files.set(fi.key, fi.value);
-            }
-        });
-        return files;
-    }
+	static public function main() {
+		var data = Sys.args()[0];
+		var content = File.getContent(data);
+		var obj:src.DataFormat = haxe.Json.parse(content);
+
+		Sys.println("Starting observation...");
+
+		while (true) {
+			if (AFileWasSaved(obj.path, obj.extension)) {
+				Sys.println("File change detected. Compiling...");
+				Sys.command(obj.command);
+				Sys.println("Done.");
+			}
+			now = Date.now();
+
+			Sys.sleep(obj.delay);
+		}
+	}
+
+	static private function AFileWasSaved(path:String, extension:Array<String>):Bool {
+		var filePaths = FileSystem.readDirectory(path);
+
+		for (filePath in filePaths) {
+			var trueFilePath = '${path}/${filePath}';
+			if (!FileSystem.isDirectory(trueFilePath) && extension.has(Path.extension(trueFilePath))) {
+				if (FileSystem.stat(trueFilePath).mtime.getTime() > now.getTime()) {
+					return true;
+				}
+			}
+		}
+
+		for (filePath in filePaths) {
+			var trueFilePath = '${path}/${filePath}';
+			if (FileSystem.isDirectory(trueFilePath)) {
+				if (AFileWasSaved(trueFilePath, extension)) {
+					return false;
+				}
+			}
+		}
+
+		return false;
+	}
 }
